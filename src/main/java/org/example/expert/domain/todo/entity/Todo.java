4dbcadc5f4a32372ccb05a45f40ced0cf4476eba
jiiimni <leejimin8951@naver.com>
2026-04-06 19:03:18 +0900

    feat: Todo 생성 시 작성자를 담당자로 자동 등록하도록 cascade 적용

--- src/main/java/org/example/expert/domain/todo/entity/Todo.java
+++ src/main/java/org/example/expert/domain/todo/entity/Todo.java
@@ -17,8 +17,10 @@ import java.util.List;
 @Table(name = "todos")
 public class Todo extends Timestamped {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String contents;
     private String weather;
@@ -30,7 +32,9 @@ public class Todo extends Timestamped {
     @OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
     private List<Comment> comments = new ArrayList<>();
 
-    @OneToMany(mappedBy = "todo")
+    @OneToMany(mappedBy = "todo", cascade = CascadeType.PERSIST)
+    // 수정: Todo 저장 시 managers 리스트에 있는 Manager도 함께 DB에 저장되도록 cascade 설정 추가
+    // 개념 정리: cascade = PERSIST → 부모(Todo) 저장 시 자식(Manager)도 같이 저장됨
     private List<Manager> managers = new ArrayList<>();
 
     public Todo(String title, String contents, String weather, User user) {
@@ -38,6 +42,10 @@ public class Todo extends Timestamped {
         this.contents = contents;
         this.weather = weather;
         this.user = user;
+
+        // 수정 포인트 설명:
+        // Todo 생성 시 작성자를 자동으로 담당자로 등록하기 위해 Manager 객체를 생성해서 리스트에 추가
+        // 위에서 cascade = PERSIST 설정을 했기 때문에 todoRepository.save(todo)만 해도 이 Manager도 같이 저장됨
         this.managers.add(new Manager(user, this));
     }
 }
